38aaf64125dc8a9b2696dd9da2fd68d86d8b3281
Koji AGAWA <atty303@gmail.com>
2019-07-24 08:00:53 +0900

    specify version

--- mask.rb
+++ mask.rb
@@ -3,11 +3,13 @@ require "formula"
 class Mask < Formula
   desc "A CLI task runner defined by a simple markdown file"
   homepage "https://github.com/jakedeichert/mask"
-  url "https://crates.io/crates/mask"
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "0.3.1"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "mask", "--root", prefix, "--path", "."
+    system "cargo", "install", "mask", "--root", prefix, "--path", ".", "--version", version
   end
 end
