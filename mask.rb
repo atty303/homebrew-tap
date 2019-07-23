7d964aa7cc59ce1ac5e6d39b3855d2d33904f89a
Koji AGAWA <atty303@gmail.com>
2019-07-24 07:27:17 +0900

    added url

--- mask.rb
+++ mask.rb
@@ -3,6 +3,7 @@ require "formula"
 class Mask < Formula
   desc "A CLI task runner defined by a simple markdown file"
   homepage "https://github.com/jakedeichert/mask"
+  url "https://crates.io/crates/mask"
 
   depends_on "rust" => :build
 
