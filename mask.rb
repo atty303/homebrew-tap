e8e602b773ab33035fbc325b7f236515ed471ab6
Koji AGAWA <atty303@gmail.com>
2019-07-28 07:57:36 +0900

    mask 0.4.0

--- mask.rb
+++ mask.rb
@@ -3,13 +3,12 @@ require "formula"
 class Mask < Formula
   desc "A CLI task runner defined by a simple markdown file"
   homepage "https://github.com/jakedeichert/mask"
-  url "https://github.com/atty303/mask/archive/v0.3.1-p1.tar.gz"
-  sha256 "fedec03865b9c12f15540bb10935e7b664c4fa131e8d64ccadcec7512a5742c5"
-  version "0.3.1-p1"
+  url "file:///dev/null"
+  version "0.4.0"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "mask", "--root", prefix, "--path", "."
+    system "cargo", "install", "mask", "--root", prefix, "--version", version
   end
 end
