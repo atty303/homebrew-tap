a44fa6f7204643a6c40e2d9494fbb4d959dbc82c
Koji AGAWA <atty303@gmail.com>
2019-07-28 08:01:00 +0900

    added sha256 for mask

--- mask.rb
+++ mask.rb
@@ -5,6 +5,7 @@ class Mask < Formula
   homepage "https://github.com/jakedeichert/mask"
   url "file:///dev/null"
   version "0.4.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   depends_on "rust" => :build
 
