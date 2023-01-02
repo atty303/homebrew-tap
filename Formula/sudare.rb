ef53d79bd8206d553d24c44a809daad5403cae25
atty303 <atty303@users.noreply.github.com>
2023-01-02 14:17:29 +0000

    Bump version to 0.1.4

--- Formula/sudare.rb
+++ Formula/sudare.rb
@@ -1,20 +1,20 @@
 class Sudare < Formula
   desc "Manage Procfile-based applications with terminal multiplexer."
   homepage "https://github.com/atty303/sudare"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/atty303/sudare/releases/download/v0.1.3/sudare-aarch64-apple-darwin.tar.gz"
-      sha256 "373893a56fa611bd11e8c92d373a08e8502b9babe74e38f8a9fb1f52ad610337"
+      url "https://github.com/atty303/sudare/releases/download/v0.1.4/sudare-aarch64-apple-darwin.tar.gz"
+      sha256 "99030e011102ab76e3773cc6058c0478b4aa496bf20c495bee067b9938583230"
 
       def install
         bin.install "sudare"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/atty303/sudare/releases/download/v0.1.3/sudare-x86_64-apple-darwin.tar.gz"
-      sha256 "41b533468c7151300c67002b414bd848bc30d471898ae05ad2b14761cedefae7"
+      url "https://github.com/atty303/sudare/releases/download/v0.1.4/sudare-x86_64-apple-darwin.tar.gz"
+      sha256 "60409c91db9dac3ecc8318c6e0b4d49f390e6a22b8ef75d7fb256023c2d42e84"
 
       def install
         bin.install "sudare"
@@ -24,8 +24,8 @@ class Sudare < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/atty303/sudare/releases/download/v0.1.3/sudare-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "340239f89be2afdd825ef8b63c6a4a7cbc576fa107a8e540cf5f09fe5d60a8a1"
+      url "https://github.com/atty303/sudare/releases/download/v0.1.4/sudare-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "4c6e72e314a1f46e09dfe08f68038252ad9b5420f5ee5bb466695ba35991eab0"
 
       def install
         bin.install "sudare"
